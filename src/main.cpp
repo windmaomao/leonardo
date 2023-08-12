a33b127321c2594dc19485ab6a463b460fa8037c
Fang Jin <windmaomao@gmail.com>
2023-08-12 14:22:02 -0400

    refactor lights into onLights handler

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 int serialIn;
 // Current lights
 uint8_t lightsOn;
+// Buzz pin
+int buzzPin = 4;
+// Buzz keys & notes
+const uint8_t keys[] = {'a', 'w', 's', 'e', 'd', 'r', 'f', 't', 'g'};
+const int notes[] = {261, 277, 294, 311, 330, 349, 370, 392, 415};
 
 void setup()
 {
@@ -11,17 +16,23 @@ void setup()
   DDRB = 0xff;
   // Serial port
   Serial.begin(9600);
+  // Buzz port
+  pinMode(buzzPin, OUTPUT);
+}
+
+void onLights()
+{
+  lightsOn = (serialIn & 0xff) << 1;
+
+  PORTB = lightsOn;
 }
 
 void loop()
 {
-  // lightsOn = rand() % 256;
-
   if (Serial.available() > 0)
   {
     serialIn = Serial.read();
-    lightsOn = (serialIn & 0xff) << 1;
   }
 
-  PORTB = lightsOn;
+  onLights();
 }
