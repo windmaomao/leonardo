fde826240732e2811e64da1cf21cc6156da91f51
Fang Jin <windmaomao@gmail.com>
2023-08-12 14:06:44 -0400

    listen to serial to turn lights on

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
 
+// Incoming serial data
+int serialIn;
+// Current lights
+uint8_t lightsOn;
+
 void setup()
 {
+  // Ports for lights
   DDRB = 0xff;
+  // Serial port
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  PORTB = 0b01100000;
+  // lightsOn = rand() % 256;
+
+  if (Serial.available() > 0)
+  {
+    serialIn = Serial.read();
+    lightsOn = (serialIn & 0xff) << 1;
+  }
+
+  PORTB = lightsOn;
 }
