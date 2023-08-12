a507839c5297a7bb4826b12232c646900beb278d
Fang Jin <windmaomao@gmail.com>
2023-08-12 17:04:05 -0400

    make a buzz out of the serial input directly

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,6 @@ int serialIn;
 uint8_t lightsOn;
 // Buzz pin
 int buzzPin = 4;
-// Buzz keys & notes
-const uint8_t keys[] = {'a', 'w', 's', 'e', 'd', 'r', 'f', 't', 'g'};
-const int notes[] = {261, 277, 294, 311, 330, 349, 370, 392, 415};
 
 void setup()
 {
@@ -29,13 +26,13 @@ void onLights()
 
 void onBuzz()
 {
-  size_t i;
-  for (i = 0; i < sizeof(keys); i++)
+  if (serialIn > 35)
   {
-    if (serialIn == keys[i])
-    {
-      tone(buzzPin, notes[i]);
-    }
+    tone(buzzPin, serialIn << 2);
+  }
+  else
+  {
+    noTone(buzzPin);
   }
 }
 
