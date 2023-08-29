b370bc552f61885deb6d159ff091f6adcba6bd2f
Fang Jin <fang.jin@pearson.com>
2023-08-29 15:50:30 -0400

    support select key via long press

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,24 @@
 
 // Buzz pin
 int buzzPin = 4;
+
 // Key switches
 const int keysCount = 2;
 Button keySwitches[keysCount] = {Button(7), Button(9)};
-// Rotary pins
-// Button leftSpin(6);
-// Button rightSpin(5);
-// Button confirmButton(8);
+uint32_t lastPressTimes[2] = {0, 0};
+
 // Oled display
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
-// Keycode
-int keycode = KEY_ESC;
-// Selected key
-int selected = 0;
+
 // Rotary control
 ClickEncoder rotary(5, 6, 8);
-int16_t value;
 void timerIsr() {
   rotary.service();
 }
+
+// Current keycode
+int keycode = KEY_ESC;
+
 
 void setup()
 {
@@ -48,10 +47,6 @@ void setup()
   }
 
   pinMode(buzzPin, OUTPUT);
-
-  // leftSpin.begin();
-  // rightSpin.begin();
-  // confirmButton.begin();
 
   Timer1.initialize(1000);
   Timer1.attachInterrupt(timerIsr);
@@ -72,21 +67,30 @@ void loop()
 {
   // handle key switches
   char keyId[3];
+  int tmpCode;
   for (int i = 0; i < keysCount; i++)
   {
     sprintf(keyId, "<%d>", i + 1);
     keySwitches[i].read();
     if (keySwitches[i].wasPressed())
     {
-      keycode = EEPROM.read(i);
-      sendKey(keycode, false);
-      printKey(keycode, keyId);
+      tmpCode = EEPROM.read(i);
+      sendKey(tmpCode, false);
+      printKey(tmpCode, "|");
+      lastPressTimes[i] = millis();
     }
     if (keySwitches[i].wasReleased())
     {
-      sendKey(keycode, true);
-      printKey(keycode, "");
-      selected = i;
+      // long press
+      if (millis() - lastPressTimes[i] > 1000) {
+        EEPROM.update(i, keycode);
+        printKey(keycode, "o");
+        buzzTone(500);
+      } else {
+        tmpCode = EEPROM.read(i);
+        sendKey(tmpCode, true);
+        printKey(tmpCode, "");
+      }
     }
   }
 
@@ -97,13 +101,6 @@ void loop()
     keycode = constrain(keycode, 1, 255);
     printKey(keycode, "");
   }
-
-  // // handle confirm button
-  // confirmButton.read();
-  // if (confirmButton.wasReleased())
-  // {
-  //   EEPROM.update(selected, keycode);
-  // }
 }
 
 void sendKey(int key, bool release)
