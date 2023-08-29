9a0595fafd4c6c5d861ff5ac0f83c55afcadee09
Fang Jin <fang.jin@pearson.com>
2023-08-29 14:58:01 -0400

    apply click rotary for accuracy

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,54 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
+#include <ClickEncoder.h>
+#include <TimerOne.h>
+
 // Buzz pin
 int buzzPin = 4;
 // Key switches
 const int keysCount = 2;
 Button keySwitches[keysCount] = {Button(7), Button(9)};
 // Rotary pins
-Button leftSpin(6);
-Button rightSpin(5);
-Button confirmButton(8);
+// Button leftSpin(6);
+// Button rightSpin(5);
+// Button confirmButton(8);
 // Oled display
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
 // Keycode
 int keycode = KEY_ESC;
 // Selected key
 int selected = 0;
+// Rotary control
+ClickEncoder rotary(5, 6, 8);
+int16_t value;
+void timerIsr() {
+  rotary.service();
+}
 
 void setup()
 {
   setupSettings();
+
   Serial.begin(9600);
+
   Keyboard.begin();
   for (int i = 0; i < keysCount; i++)
   {
     keySwitches[i].begin();
   }
+
   pinMode(buzzPin, OUTPUT);
-  leftSpin.begin();
-  rightSpin.begin();
-  confirmButton.begin();
+
+  // leftSpin.begin();
+  // rightSpin.begin();
+  // confirmButton.begin();
+
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr);
+  rotary.setAccelerationEnabled(1);
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   displayText("Ready.");
 }
@@ -51,8 +70,8 @@ void printKey(int key, const char *info)
 
 void loop()
 {
-  char keyId[3];
   // handle key switches
+  char keyId[3];
   for (int i = 0; i < keysCount; i++)
   {
     sprintf(keyId, "<%d>", i + 1);
@@ -71,39 +90,20 @@ void loop()
     }
   }
 
-  // handle rotary spin
-  bool l = leftSpin.read();
-  bool r = rightSpin.read();
-  if (leftSpin.wasReleased() || leftSpin.wasPressed())
-  {
-    if (l == r)
-    {
-      keycode--;
-      if (keycode < 0)
-      {
-        keycode = 255;
-      }
-      buzzTone(1000);
-      printKey(keycode, "<-");
-    }
-    else
-    {
-      keycode++;
-      if (keycode > 255)
-      {
-        keycode = 0;
-      }
-      buzzTone(2000);
-      printKey(keycode, "->");
-    }
+  // handle rotary
+  int inc = rotary.getValue();
+  if (inc != 0) {
+    keycode += inc;
+    keycode = constrain(keycode, 1, 255);
+    printKey(keycode, "");
   }
 
-  // handle confirm button
-  confirmButton.read();
-  if (confirmButton.wasReleased())
-  {
-    EEPROM.update(selected, keycode);
-  }
+  // // handle confirm button
+  // confirmButton.read();
+  // if (confirmButton.wasReleased())
+  // {
+  //   EEPROM.update(selected, keycode);
+  // }
 }
 
 void sendKey(int key, bool release)
