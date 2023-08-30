a28ddd99e2d14e9cb3c8e1d89dea83d41fdb1f14
Fang Jin <fang.jin@pearson.com>
2023-08-30 14:13:47 -0400

    support media mode

--- src/main.hpp
+++ src/main.hpp
@@ -1,4 +1,8 @@
 void buzzTone(unsigned int freq);
 void displayText(const char *text);
+void printKey(int key, const char *info);
 void sendKey(int key, bool release);
 void setupSettings();
+void loopNormalMode();
+void loopMediaMode();
+void printMode(int m);
